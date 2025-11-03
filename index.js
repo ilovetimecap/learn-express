b451f62b70aacf2b10db96e4eff358a5b32abd14
ilovetimecap <a8756105@naver.com>
2025-11-03 14:16:07 +0900

    env추가

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express"); // express 가져오기
+const dotenv = require("dotenv");
+
 const app = express();
+
+//환경변수 로드
+ dotenv.config()
 
 // 서버 실행
 app.listen(8080, () => {
-    console.log("server running")
+    console.log("server running",
+    process.env.PORT)
 })
